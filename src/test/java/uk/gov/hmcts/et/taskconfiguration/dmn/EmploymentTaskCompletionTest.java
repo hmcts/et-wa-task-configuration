73b67ab523a89a69a65400b51efe35f5602dc30e
fergus.oshea <fergus.oshea@version1.com>
2023-05-18 17:26:19 +0100

    RET-2348: Camunda & Unit Tests

--- src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCompletionTest.java
@@ -47,6 +47,24 @@ class EmploymentTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                         "completionMode", "Auto"
                     )
                 )
+            ),
+            Arguments.of(
+                "replyToReferral",
+                asList(
+                    Map.of(
+                        "taskType", "ReviewReferralAdmin",
+                        "completionMode", "Auto"
+                    )
+                )
+            ),
+            Arguments.of(
+                "closeReferral",
+                asList(
+                    Map.of(
+                        "taskType", "ReviewReferralAdmin",
+                        "completionMode", "Auto"
+                    )
+                )
             )
         );
     }
@@ -65,6 +83,6 @@ class EmploymentTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(2));
+        assertThat(logic.getRules().size(), is(4));
     }
 }
