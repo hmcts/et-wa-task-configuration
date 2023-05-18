b0ab15639428a42b966311455f8078b6b31b0927
Paul.Peasegood@hmcts.net <paulp@LAPTOP4439.V1.com>
2023-05-18 15:35:08 +0100

    RET-2436, RET-2437 IssueInitialConsiderationDirections

--- src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCompletionTest.java
@@ -47,6 +47,15 @@ class EmploymentTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                         "completionMode", "Auto"
                     )
                 )
+            ),
+            Arguments.of(
+                "IssueInitialConsiderationDirections",
+                asList(
+                    Map.of(
+                        "taskType", "IssueInitialConsiderationDirections",
+                        "completionMode", "Auto"
+                    )
+                )
             )
         );
     }
@@ -65,6 +74,6 @@ class EmploymentTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(2));
+        assertThat(logic.getRules().size(), is(3));
     }
 }
