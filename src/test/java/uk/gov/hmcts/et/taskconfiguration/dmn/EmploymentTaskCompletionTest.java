7d449919e5cfe8e4e943f41f297095e3911f9ca8
fergus.oshea <fergus.oshea@version1.com>
2023-05-24 17:16:00 +0100

    RET-2370: Post Merge Tidy up

--- src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCompletionTest.java
@@ -143,12 +143,7 @@ class EmploymentTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                     Map.of(
                         "taskType", "IssuePostHearingDirection",
                         "completionMode", "Auto"
-                    )
-                )
-            ),
-            Arguments.of(
-                "generateCorrespondence",
-                asList(
+                    ),
                     Map.of(
                         "taskType", "SendEt1Notification",
                         "completionMode", "Auto"
@@ -172,6 +167,6 @@ class EmploymentTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(13));
+        assertThat(logic.getRules().size(), is(14));
     }
 }
--- src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskInitiationTest.java
@@ -295,7 +295,7 @@ class EmploymentTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(13));
+        assertThat(logic.getRules().size(), is(14));
     }
 
     private static Map<String, Object> mapAdditionalData(String additionalData) {
