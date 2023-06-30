43c202d188fbbcd472b257883e0034a5a4a3c133
fergus.oshea <fergus.oshea@version1.com>
2023-06-30 13:00:07 +0100

    RET-3851: More updates

--- src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskConfigurationTest.java
@@ -677,7 +677,7 @@ class EmploymentTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
 
-        assertThat(logic.getRules().size(), is(43));
+        assertThat(logic.getRules().size(), is(48));
     }
 
     private static Map<String, Object> mapData(String source) {
