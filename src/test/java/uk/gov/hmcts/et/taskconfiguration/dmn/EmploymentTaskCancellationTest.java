0058f51d081829fc407708a4195e49fba4d67d63
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2023-08-15 14:22:00 +0100

    RET-4096 removed duplicate entries

--- src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCancellationTest.java
+++ src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCancellationTest.java
@@ -38,11 +38,7 @@ class EmploymentTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
                         "action", "Cancel",
                         "processCategories", "Vetting"
                     ),
-                    Map.of(
-                        "action", "Cancel",
-                        "processCategories", "Vetting"
-                    ),
-                    Map.of(
+                   Map.of(
                         "action", "Cancel",
                         "processCategories", "processing"
                     ),
@@ -52,27 +48,7 @@ class EmploymentTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
                     ),
                     Map.of(
                         "action", "Cancel",
-                        "processCategories", "Vetting"
-                    ),
-                    Map.of(
-                        "action", "Cancel",
-                        "processCategories", "processing"
-                    ),
-                    Map.of(
-                        "action", "Cancel",
                         "processCategories", "Judgment"
-                    ),
-                    Map.of(
-                        "action", "Cancel",
-                        "processCategories", "processing"
-                    ),
-                    Map.of(
-                        "action", "Cancel",
-                        "processCategories", "Vetting"
-                    ),
-                    Map.of(
-                        "action", "Cancel",
-                        "processCategories", "processing"
                     ),
                     Map.of(
                         "action", "Cancel",
@@ -136,6 +112,6 @@ class EmploymentTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(15));
+        assertThat(logic.getRules().size(), is(9));
     }
 }
