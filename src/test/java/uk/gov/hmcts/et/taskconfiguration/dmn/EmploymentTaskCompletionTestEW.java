2d01ca4624ec889e4b9e44a1cbf6c9ece2c35ddf
lahiru.hinguruduwa <oshara19@gmail.com>
2026-03-05 08:41:37 +0000

    ut fix

--- src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCompletionTestEW.java
+++ src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskCompletionTestEW.java
@@ -91,27 +91,33 @@ class EmploymentTaskCompletionTestEW extends DmnDecisionTableBaseUnitTest {
                 asList(
                     Map.of(
                         "taskType", "ReviewReferralAdmin",
-                        "completionMode", "Auto"
+                        "completionMode", "Auto",
+                        "processCategories", ""
                     ),
                     Map.of(
                         "taskType", "ReviewReferralResponseAdmin",
-                        "completionMode", "Auto"
+                        "completionMode", "Auto",
+                        "processCategories", ""
                     ),
                     Map.of(
                         "taskType", "ReviewReferralJudiciary",
-                        "completionMode", "Auto"
+                        "completionMode", "Auto",
+                        "processCategories", ""
                     ),
                     Map.of(
                         "taskType", "ReviewReferralLegalOps",
-                        "completionMode", "Auto"
+                        "completionMode", "Auto",
+                        "processCategories", ""
                     ),
                     Map.of(
                         "taskType", "ReviewReferralResponseJudiciary",
-                        "completionMode", "Auto"
+                        "completionMode", "Auto",
+                        "processCategories", ""
                     ),
                     Map.of(
                         "taskType", "ReviewReferralResponseLegalOps",
-                        "completionMode", "Auto"
+                        "completionMode", "Auto",
+                        "processCategories", ""
                     ),
                     emptyMap()
                 )
@@ -334,6 +340,6 @@ class EmploymentTaskCompletionTestEW extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(29));
+        assertThat(logic.getRules().size(), is(35));
     }
 }
