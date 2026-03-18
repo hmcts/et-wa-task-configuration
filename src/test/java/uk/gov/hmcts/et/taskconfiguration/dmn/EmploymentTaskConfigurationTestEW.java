f9deaf927f02857e2df68a6343c0d3d09fb69d02
btensay <tensay.bulcha@gmail.com>
2026-03-18 09:38:38 +0000

    ret-6217 partial implementation

--- src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskConfigurationTestEW.java
+++ src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskConfigurationTestEW.java
@@ -1104,7 +1104,7 @@ class EmploymentTaskConfigurationTestEW extends DmnDecisionTableBaseUnitTest {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
 
-        assertThat(logic.getRules().size(), is(73));
+        assertThat(logic.getRules().size(), is(74));
     }
 
     private List<Map<String, Object>> getExpectedValues() {
