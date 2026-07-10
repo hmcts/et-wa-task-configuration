3045504361dc5960abd5f412605a6f41f19aca7f
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2026-07-10 09:12:27 +0100

    Update majorPriority

--- src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskConfigurationTestEW.java
+++ src/test/java/uk/gov/hmcts/et/taskconfiguration/dmn/EmploymentTaskConfigurationTestEW.java
@@ -1170,7 +1170,7 @@ class EmploymentTaskConfigurationTestEW extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(73));
+        assertThat(logic.getRules().size(), is(74));
     }
 
     private List<Map<String, Object>> getExpectedValues() {
